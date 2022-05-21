019e115d82b9c4d227b075e2d26e39f054572036
Browsero <hubertmadej02@gmail.com>
2022-05-21 16:56:54 +0200

    Strapi Restaurant Advisor

---
+++ src/components/Menu.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+import { bool } from 'prop-types';
+import { Link } from "react-router-dom";
+
+const Menu = ({ open }) => {
+  return (
+      <StyledMenu open={open} >
+        <p><Link to='/'>Home</Link></p>
+        <p><Link to='/restaurants'>Restaurants</Link></p>
+        <p><Link to='/contact'>Contact</Link></p>
+      </StyledMenu>
+  );
+};
+Menu.propTypes = {
+    open: bool.isRequired,
+  }
+export default Menu;
+
+const StyledMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #ABE188;
+  height: 100%;
+  text-align: left;
+  padding: 2rem;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: transform 0.3s ease-in-out;
+  transform: translateY(-100%);
+  transform: ${({ open }) => open ? 'translateY(0)' : 'translateY(-100%)'};
+
+  p {
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: 500;
+    letter-spacing: 0.5rem;
+    text-decoration: none;
+    transition: color 0.3s linear;
+    color: white;
+  }
+`;
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Burger from "./Burger";
+import Menu from "./Menu";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="flex justify-between items-center p-8 shadow-sm">
+      <div className="flex items-center gap-4 cursor-default">
+        <img
+          className="w-16"
+          alt="logo"
+          src={process.env.PUBLIC_URL + "/restaurant.png"}
+        />
+        <h1 className="hidden md:w-32 ">
+          Restaurant <span className="bg-red-50 p-2 rounded-lg">Advisor</span>
+        </h1>
+      </div>
+      <div className="md:hidden">
+          <Burger open={open} setOpen={setOpen} />
+          <Menu open={open} setOpen={setOpen} />
+        </div>
+        <ul className="hidden md:flex md:gap-8 md:mr-16">
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/restaurants'>Restaurants</Link></li>
+        <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
